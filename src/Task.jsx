c3ec65d8477c97caebda4b75231e385d1cddd75f
Himani1608 <94044843+Himani1608@users.noreply.github.com>
2023-02-03 23:15:22 +0530

    Update Task.jsx

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,19 +1,11 @@
 import React, { useState } from 'react'
 
 function Task(props) {
-  const[isStriked , setStrike]= useState('false');
-
-  const handleCheck = () => {
-    if(isStriked === false){
-        setStrike(true)
-    }else{
-      setStrike(false)
-    }
-  }
-
+  const[isStriked , setStrike]= useState(false);
+  
   return (
     <div className='list-items'>
-        <input type="checkbox" checked = {isStriked} onClick={handleCheck} />
+        <input type="checkbox" checked = {isStriked} onClick={() => {setStrike(!isStriked)}} />
         <li style={{textDecoration: isStriked && 'line-through'}}>{props.value}</li>
         <i className='fa fa-times-circle' onClick={() => {props.sendData(props.id)}}></i>
     </div>
